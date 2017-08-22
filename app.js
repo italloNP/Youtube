89f563e8344bfac8835e20c9289d82584122d823
Itallo <itallonobregag@gmail.com>
2017-08-22 08:17:23 -0300

    Public key

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function resetVideoHeight() {
 }
 
 function init() {
-    gapi.client.setApiKey("YOUR_PUBLIC_KEY");
+    gapi.client.setApiKey("AIzaSyALF_VqpV5wnp5rqUXP5hzsvBe6iUitHGc");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
     });
